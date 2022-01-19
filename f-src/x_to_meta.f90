3d9adc2b62443c926cbb43579019e4ff7e552d83
JoGebert <johannes.gebert@hlrs.de>
2022-01-20 00:33:45 +0100

    Serial XTOm

--- f-src/x_to_meta.f90
+++ f-src/x_to_meta.f90
@@ -4,14 +4,13 @@ PROGRAM xtometa
 !
 !> @author Johannes Gebert - HLRS - NUM - gebert@hlrs.de
 !> Date:    12.09.2021
-!> LastMod: 28.12.2021
+!> LastMod: 20.01.2022
 !>-------------------------------------------------------------------
 
 USE ISO_FORTRAN_ENV
 USE global_std
 USE user_interaction
 USE meta
-USE MPI
 USE vtk_meta_data
 USE raw_binary  
 
@@ -19,17 +18,12 @@ IMPLICIT NONE
 ! Parameter
 INTEGER(KIND=ik), PARAMETER :: debug = 1
 
-! MPI variables
-INTEGER(KIND=mik) :: ierr, my_rank, size_mpi
-
 ! Std variables
 CHARACTER(LEN=scl) :: type_in, type_out, binary, restart, restart_cmd_arg
 CHARACTER(LEN=mcl) :: filename=''
 
 INTEGER(KIND=ik) :: hdr
-INTEGER(KIND=mik), DIMENSION(3) :: sections
-INTEGER(KIND=ik), DIMENSION(3) :: dims, rry_dims, rank_section, sections_ik
-INTEGER(KIND=ik), DIMENSION(3) :: remainder_per_dir, dims_reduced, subarray_origin
+INTEGER(KIND=ik), DIMENSION(3) :: dims
 REAL   (KIND=rk), DIMENSION(3) :: spcng, origin
 REAL   (KIND=rk) :: start, end
 
@@ -41,168 +35,106 @@ INTEGER(KIND=INT32) , DIMENSION(:,:,:), ALLOCATABLE :: rry_ik4
 
 LOGICAL :: stp = .FALSE.
 
-!------------------------------------------------------------------------------
-! Invoke MPI 
-!------------------------------------------------------------------------------
-CALL mpi_init(ierr)
-CALL print_err_stop(std_out, "MPI_INIT didn't succeed", INT(ierr, KIND=ik))
-
-CALL MPI_COMM_RANK(MPI_COMM_WORLD, my_rank, ierr)
-CALL print_err_stop(std_out, "MPI_COMM_RANK couldn't be retrieved", INT(ierr, KIND=ik))
-
-CALL MPI_COMM_SIZE(MPI_COMM_WORLD, size_mpi, ierr)
-CALL print_err_stop(std_out, "MPI_COMM_SIZE couldn't be retrieved", INT(ierr, KIND=ik))
-
-IF (size_mpi < 2) CALL print_err_stop(std_out, "We need at least 2 MPI processes to execute this program.", 1)
+CALL CPU_TIME(start)
 
 !------------------------------------------------------------------------------
-! Rank 0 -- Init (Master) Process and broadcast init parameters 
+! Parse the command arguments
 !------------------------------------------------------------------------------
-IF (my_rank==0) THEN
+CALL get_cmd_args(binary, filename, stp, restart, restart_cmd_arg)
+IF(stp) GOTO 1001
 
-    CALL CPU_TIME(start)
-
-    !------------------------------------------------------------------------------
-    ! Parse the command arguments
-    !------------------------------------------------------------------------------
-    CALL get_cmd_args(binary, filename, stp, restart, restart_cmd_arg)
-    IF(stp) GOTO 1001
-    
-    IF (filename=='') THEN
-        CALL usage(binary)    
-
-        !------------------------------------------------------------------------------
-        ! On std_out since file of std_out is not spawned
-        !------------------------------------------------------------------------------
-        CALL print_err_stop(6, "No input file given", 1)
-    END IF
-
-    CALL meta_create_new(TRIM(ADJUSTL(filename)))
+IF (filename=='') THEN
+    CALL usage(binary)    
 
     !------------------------------------------------------------------------------
-    ! Redirect std_out into a file in case std_out is not useful by environment.
-    ! Place these lines before handle_lock_file :-)
+    ! On std_out since file of std_out is not spawned
     !------------------------------------------------------------------------------
-    std_out = determine_stout()
+    CALL print_err_stop(6, "No input file given", 1)
+END IF
 
-    !------------------------------------------------------------------------------
-    ! Spawn standard out after(!) the basename is known
-    !------------------------------------------------------------------------------
-    IF(std_out/=6) CALL meta_start_ascii(std_out, '.std_out')
-
-    CALL show_title()
- 
-    IF(debug >=0) WRITE(std_out, FMT_MSG) "Post mortem info probably in ./datasets/.temporary.std_out"
-
-    !------------------------------------------------------------------------------
-    ! Read VTK file header
-    !------------------------------------------------------------------------------
-    WRITE(std_out, FMT_TXT) 'Reading the vtk header.'
-    CALL read_vtk_meta (filename, hdr, dims, origin, spcng, type_in) 
-
-    !------------------------------------------------------------------------------
-    ! Determine data types
-    ! Data types are converted to integer, since the provided precision on the
-    ! data is sufficient.
-    !------------------------------------------------------------------------------
-    WRITE(std_out, FMT_TXT) 'Determining the output data types.'
-    SELECT CASE(type_in)
-        CASE('rk4') ; type_out = 'ik4'
-        CASE('rk8') ; type_out = 'ik4'
-        CASE('ik4') ; type_out = 'ik4'
-        CASE('ik2') ; type_out = 'ik2'
-        CASE('uik2'); type_out = 'ik4'
-    END SELECT
-
-    !------------------------------------------------------------------------------
-    ! Write meta data 
-    !------------------------------------------------------------------------------
-    WRITE(std_out, FMT_TXT) 'Writing meta information to *.meta file.'
-
-    WRITE(fhmeo, '(A)') "# HLRS|NUM Dataset Meta Information"
-    WRITE(fhmeo, '(A)') ""
-    WRITE(fhmeo, '(A)') "* GENERAL_INFORMATION"
-    CALL meta_write (fhmeo, 'CT_SCAN', in%dataset)
-    CALL meta_write (fhmeo, 'OWNER',         "TBD by user")
-    CALL meta_write (fhmeo, 'OWNER_CONTACT', "TBD by user")
-    CALL meta_write (fhmeo, 'DATE_CREATED',  "TBD by user")
-    CALL meta_write (fhmeo, 'INTERNAL_ID',   "TBD by user")
-    CALL meta_write (fhmeo, 'HISTORY', '(-)' , 1)
-    
-    ! Write original data type to file for documentaroy purposes
-    CALL meta_write (fhmeo, 'TYPE_IMPORT', TRIM(ADJUSTL(type_in)))
-    CALL meta_write (fhmeo, 'TYPE_RAW', TRIM(ADJUSTL(type_out)))
-    
-    !------------------------------------------------------------------------------
-    ! Data always are written as little endian; only vtk requires big endian.
-    !------------------------------------------------------------------------------
-    CALL meta_write (fhmeo, 'DATA_BYTE_ORDER'  , "BigEndian")
-    CALL meta_write (fhmeo, 'DIMENSIONALITY'   , '(-)'  , 3)
-    CALL meta_write (fhmeo, 'DIMENSIONS'       , '(-)'  , dims)
-    CALL meta_write (fhmeo, 'NO_SCALAR_CMPNNTS', '(-)'  , 1)
-    CALL meta_write (fhmeo, 'SPACING'          , '(mm)' , spcng)
-    CALL meta_write (fhmeo, 'ORIGIN_SHIFT_GLBL', '(mm)' , [0._rk, 0._rk, 0._rk])
-    CALL meta_write (fhmeo, 'ORIGIN'           , '(-)'  , [0, 0, 0])
-    CALL meta_write (fhmeo, 'FIELD_OF_VIEW'    , '(mm)' , dims*spcng)
-    CALL meta_write (fhmeo, 'ENTRIES'          , '(-)'  , PRODUCT(dims))
-   
-    FLUSH(fhmeo)
-
-END IF ! my_rank==0
+CALL meta_create_new(TRIM(ADJUSTL(filename)))
 
 !------------------------------------------------------------------------------
-! Send required variables
+! Redirect std_out into a file in case std_out is not useful by environment.
+! Place these lines before handle_lock_file :-)
 !------------------------------------------------------------------------------
-CALL MPI_BCAST (type_in     , INT(scl, KIND=mik)     , MPI_CHAR, 0_mik, MPI_COMM_WORLD, ierr)
-CALL MPI_BCAST (type_out    , INT(scl, KIND=mik)     , MPI_CHAR, 0_mik, MPI_COMM_WORLD, ierr)
-CALL MPI_BCAST (in%p_n_bsnm , INT(meta_mcl, KIND=mik), MPI_CHAR, 0_mik, MPI_COMM_WORLD, ierr)
-CALL MPI_BCAST (out%p_n_bsnm, INT(meta_mcl, KIND=mik), MPI_CHAR, 0_mik, MPI_COMM_WORLD, ierr)
-CALL MPI_BCAST (hdr , 1_mik, MPI_INTEGER8, 0_mik, MPI_COMM_WORLD, ierr)
-CALL MPI_BCAST (dims, 3_mik, MPI_INTEGER8, 0_mik, MPI_COMM_WORLD, ierr)
+std_out = determine_stout()
 
 !------------------------------------------------------------------------------
-! Get dimensions for each domain. Every processor reveives its own domain.
-! Therefore, each my_rank calculates its own address/dimensions/parameters.
-!
-! The decomposition of an image depends on the task to be done. Therefore, 
-! these calculations are not encapsuled in a routine. However, this may
-! follow in an update during refactoring the 3D scalar image filter.
-!
-! Allocation of subarray memory is done in the read_raw routines.
+! Spawn standard out after(!) the basename is known
 !------------------------------------------------------------------------------
-sections=0
-CALL MPI_DIMS_CREATE (size_mpi, 3_mik, sections, ierr)
-CALL get_rank_section(INT(my_rank, KIND=ik), INT(sections, KIND=ik), rank_section)
+IF(std_out/=6) CALL meta_start_ascii(std_out, '.std_out')
 
-sections_ik = INT(sections, KIND=ik)
-remainder_per_dir = MODULO(dims, sections_ik)
+CALL show_title()
 
-dims_reduced   = dims - remainder_per_dir
+IF(debug >=0) WRITE(std_out, FMT_MSG) "Post mortem info probably in ./datasets/.temporary.std_out"
 
-rry_dims  = (dims_reduced / sections_ik)
+!------------------------------------------------------------------------------
+! Read VTK file header
+!------------------------------------------------------------------------------
+WRITE(std_out, FMT_TXT) 'Reading the vtk header.'
+CALL read_vtk_meta (filename, hdr, dims, origin, spcng, type_in) 
 
-subarray_origin = (rank_section-1_ik) * (rry_dims)
+!------------------------------------------------------------------------------
+! Determine data types
+! Data types are converted to integer, since the provided precision on the
+! data is sufficient.
+!------------------------------------------------------------------------------
+WRITE(std_out, FMT_TXT) 'Determining the output data types.'
+SELECT CASE(type_in)
+    CASE('rk4') ; type_out = 'ik4'
+    CASE('rk8') ; type_out = 'ik4'
+    CASE('ik4') ; type_out = 'ik4'
+    CASE('ik2') ; type_out = 'ik2'
+    CASE('uik2'); type_out = 'ik4'
+END SELECT
 
-! Add the remainder to the last domains of each dimension
-IF(rank_section(1) == sections_ik(1)) rry_dims(1) = rry_dims(1) + remainder_per_dir(1)
-IF(rank_section(2) == sections_ik(2)) rry_dims(2) = rry_dims(2) + remainder_per_dir(2)
-IF(rank_section(3) == sections_ik(3)) rry_dims(3) = rry_dims(3) + remainder_per_dir(3)
+!------------------------------------------------------------------------------
+! Write meta data 
+!------------------------------------------------------------------------------
+WRITE(std_out, FMT_TXT) 'Writing meta information to *.meta file.'
+
+WRITE(fhmeo, '(A)') "# HLRS|NUM Dataset Meta Information"
+WRITE(fhmeo, '(A)') ""
+WRITE(fhmeo, '(A)') "* GENERAL_INFORMATION"
+CALL meta_write (fhmeo, 'CT_SCAN', in%dataset)
+CALL meta_write (fhmeo, 'OWNER',         "TBD by user")
+CALL meta_write (fhmeo, 'OWNER_CONTACT', "TBD by user")
+CALL meta_write (fhmeo, 'DATE_CREATED',  "TBD by user")
+CALL meta_write (fhmeo, 'INTERNAL_ID',   "TBD by user")
+CALL meta_write (fhmeo, 'HISTORY', '(-)' , 1)
+
+! Write original data type to file for documentaroy purposes
+CALL meta_write (fhmeo, 'TYPE_IMPORT', TRIM(ADJUSTL(type_in)))
+CALL meta_write (fhmeo, 'TYPE_RAW', TRIM(ADJUSTL(type_out)))
+
+!------------------------------------------------------------------------------
+! Data always are written as little endian; only vtk requires big endian.
+!------------------------------------------------------------------------------
+CALL meta_write (fhmeo, 'DATA_BYTE_ORDER'  , "LittleEndian")
+CALL meta_write (fhmeo, 'DIMENSIONALITY'   , '(-)'  , 3)
+CALL meta_write (fhmeo, 'DIMENSIONS'       , '(-)'  , dims)
+CALL meta_write (fhmeo, 'NO_SCALAR_CMPNNTS', '(-)'  , 1)
+CALL meta_write (fhmeo, 'SPACING'          , '(mm)' , spcng)
+CALL meta_write (fhmeo, 'ORIGIN_SHIFT_GLBL', '(mm)' , [0._rk, 0._rk, 0._rk])
+CALL meta_write (fhmeo, 'ORIGIN'           , '(-)'  , [0, 0, 0])
+CALL meta_write (fhmeo, 'FIELD_OF_VIEW'    , '(mm)' , dims*spcng)
+CALL meta_write (fhmeo, 'ENTRIES'          , '(-)'  , PRODUCT(dims))
+
+FLUSH(fhmeo)
 
 !------------------------------------------------------------------------------
 ! Read binary part of the vtk file - basically a *.raw file
 !------------------------------------------------------------------------------
-IF(my_rank==0) WRITE(std_out, FMT_TXT) 'Reading binary information of *.vtk file.'
+WRITE(std_out, FMT_TXT) 'Reading binary information of *.vtk file.'
+
 
 SELECT CASE(type_in)
-    CASE('rk4') 
-        ! MPI_OFFSET_KIND needs ik=8 in this case.
-        CALL mpi_read_raw(TRIM(in%p_n_bsnm)//vtk_suf, INT(hdr, KIND=8), dims, rry_dims, subarray_origin, rry_rk4)
-    CASE('rk8') 
-        CALL mpi_read_raw(TRIM(in%p_n_bsnm)//vtk_suf, INT(hdr, KIND=8), dims, rry_dims, subarray_origin, rry_rk8)
-    CASE('ik4') 
-        CALL mpi_read_raw(TRIM(in%p_n_bsnm)//vtk_suf, INT(hdr, KIND=8), dims, rry_dims, subarray_origin, rry_ik4)
+    CASE('rk4'); CALL ser_read_raw(fh_vtk, TRIM(in%p_n_bsnm)//vtk_suf, rry_rk4, 'SWAP')
+    CASE('rk8'); CALL ser_read_raw(fh_vtk, TRIM(in%p_n_bsnm)//vtk_suf, rry_rk8, 'SWAP')
+    CASE('ik4'); CALL ser_read_raw(fh_vtk, TRIM(in%p_n_bsnm)//vtk_suf, rry_ik4, 'SWAP')
     CASE('ik2', 'uik2') 
-        CALL mpi_read_raw(TRIM(in%p_n_bsnm)//vtk_suf, INT(hdr, KIND=8), dims, rry_dims, subarray_origin, rry_ik2)
+        CALL ser_read_raw(fh_vtk, TRIM(in%p_n_bsnm)//vtk_suf, rry_ik2, 'SWAP')
         IF(type_in=='uik2') THEN
             CALL uik2_to_ik4(rry_ik2, rry_ik4)
             DEALLOCATE(rry_ik2)
@@ -212,11 +144,11 @@ END SELECT
 !------------------------------------------------------------------------------
 ! Convert arrays and write raw data
 !------------------------------------------------------------------------------
-IF(my_rank==0) WRITE(std_out, FMT_TXT) 'Converting and writing binary information to *.raw file.'
+WRITE(std_out, FMT_TXT) 'Converting and writing binary information to *.raw file.'
 
 SELECT CASE(type_out)
     CASE('ik2') 
-        CALL mpi_write_raw(TRIM(out%p_n_bsnm)//raw_suf, 0_8, dims, rry_dims, subarray_origin, rry_ik2)
+        CALL ser_write_raw(fh_vtk, TRIM(out%p_n_bsnm)//raw_suf, rry_ik2)
     CASE('ik4') 
         SELECT CASE(type_in)
             CASE('rk4') 
@@ -225,7 +157,7 @@ SELECT CASE(type_out)
                 rry_ik4 = INT(rry_rk8, KIND=INT32)
         END SELECT
 
-        CALL mpi_write_raw(TRIM(out%p_n_bsnm)//raw_suf, 0_8, dims, rry_dims, subarray_origin, rry_ik4)
+        CALL ser_write_raw(fh_vtk, TRIM(out%p_n_bsnm)//raw_suf, rry_ik4)
 END SELECT
 
 !------------------------------------------------------------------------------
@@ -236,20 +168,14 @@ END SELECT
 !------------------------------------------------------------------------------
 ! Finish program
 !------------------------------------------------------------------------------
-IF(my_rank == 0) THEN
-    CALL CPU_TIME(end)
+CALL CPU_TIME(end)
 
-    WRITE(std_out, FMT_TXT_xAF0) 'Finishing the program took', end-start,'seconds.'
-    WRITE(std_out, FMT_TXT_SEP)
+WRITE(std_out, FMT_TXT_xAF0) 'Finishing the program took', end-start,'seconds.'
+WRITE(std_out, FMT_TXT_SEP)
 
-    CALL meta_signing(binary)
-    CALL meta_close(size_mpi)
+CALL meta_signing(binary)
+CALL meta_close(INT(1, KIND=INT32)) ! MPI legacy
 
-    IF (std_out/=6) CALL meta_stop_ascii(fh=std_out, suf='.std_out')
-
-END IF ! (my_rank == 0)
-
-Call MPI_FINALIZE(ierr)
-CALL print_err_stop(std_out, "MPI_FINALIZE didn't succeed", INT(ierr, KIND=ik))
+IF (std_out/=6) CALL meta_stop_ascii(fh=std_out, suf='.std_out')
 
 END PROGRAM xtometa
